1f1791c6c4577b15c498f75a624b3dd865a10834
eunseo <qnrqjrm03@naver.com>
2023-07-23 20:22:38 +0900

    [Update] BE/Clothes/74/실수로 주석안풀고 올림

--- Back/src/main/java/com/ssafy/howdoilook/domain/user/service/UserService.java
+++ Back/src/main/java/com/ssafy/howdoilook/domain/user/service/UserService.java
@@ -14,36 +14,36 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class UserService {
 
-//    private final UserRepository userRepository;
-//
-//    // 일단 에러나는데 추후 고칠 예정
-//    private final PasswordEncoder passwordEncoder;
-//
-//    /*
-//    * 일반 회원 가입
-//    * */
-//    @Transactional
-//    public Long signUp(UserSignUpRequestDto userSignUpRequestDto) throws Exception {
-//        if(userRepository.findByEmail(userSignUpRequestDto.getEmail()).isPresent())
-//            throw new Exception("이미 존재하는 이메일입니다.");
-//
-//        if(userRepository.findByNickname(userSignUpRequestDto.getNickname()).isPresent())
-//            throw new Exception("이미 존재하는 닉네임입니다.");
-//
-//        User user = User.builder()
-//                .email(userSignUpRequestDto.getEmail())
-//                .password(userSignUpRequestDto.getPassword())
-//                .name(userSignUpRequestDto.getName())
-//                .nickname(userSignUpRequestDto.getNickname())
-//                .age(userSignUpRequestDto.getAge())
-//                .gender(userSignUpRequestDto.getGender())
-//                .role(Role.USER)
-//                .build();
-//
-//        user.passwordEncode(passwordEncoder);
-//
-//        userRepository.save(user);
-//
-//        return user.getId();
-//    }
+    private final UserRepository userRepository;
+
+    // 일단 에러나는데 추후 고칠 예정
+    private final PasswordEncoder passwordEncoder;
+
+    /*
+    * 일반 회원 가입
+    * */
+    @Transactional
+    public Long signUp(UserSignUpRequestDto userSignUpRequestDto) throws Exception {
+        if(userRepository.findByEmail(userSignUpRequestDto.getEmail()).isPresent())
+            throw new Exception("이미 존재하는 이메일입니다.");
+
+        if(userRepository.findByNickname(userSignUpRequestDto.getNickname()).isPresent())
+            throw new Exception("이미 존재하는 닉네임입니다.");
+
+        User user = User.builder()
+                .email(userSignUpRequestDto.getEmail())
+                .password(userSignUpRequestDto.getPassword())
+                .name(userSignUpRequestDto.getName())
+                .nickname(userSignUpRequestDto.getNickname())
+                .age(userSignUpRequestDto.getAge())
+                .gender(userSignUpRequestDto.getGender())
+                .role(Role.USER)
+                .build();
+
+        user.passwordEncode(passwordEncoder);
+
+        userRepository.save(user);
+
+        return user.getId();
+    }
 }
