98614a47214b8f3e6c6accbcb59694f24b5416fd
ParkSeYun98 <yun12343@naver.com>
2023-07-24 13:25:02 +0900

    [Feat] BE / User 검색(아직 미완) - 일반 + Paging

--- Back/src/main/java/com/ssafy/howdoilook/domain/user/service/UserService.java
+++ Back/src/main/java/com/ssafy/howdoilook/domain/user/service/UserService.java
@@ -1,6 +1,8 @@
 package com.ssafy.howdoilook.domain.user.service;
 
+import com.ssafy.howdoilook.domain.user.dto.request.UserSearchCondition;
 import com.ssafy.howdoilook.domain.user.dto.request.UserSignUpRequestDto;
+import com.ssafy.howdoilook.domain.user.dto.response.UserSearchResponseDto;
 import com.ssafy.howdoilook.domain.user.entity.Role;
 import com.ssafy.howdoilook.domain.user.entity.SocialType;
 import com.ssafy.howdoilook.domain.user.entity.User;
@@ -8,9 +10,13 @@ import com.ssafy.howdoilook.domain.user.repository.UserRepository;
 import com.ssafy.howdoilook.global.redis.service.RedisAccessTokenService;
 import com.ssafy.howdoilook.global.redis.service.RedisRefreshTokenService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -60,5 +66,15 @@ public class UserService {
         redisAccessTokenService.setRedisAccessToken(accessToken);
         
         return "로그아웃 성공(accessToken blacklist 추가 및 refreshToken 삭제)";
+    }
+
+    public List<UserSearchResponseDto> searchUsers(UserSearchCondition userSearchCondition) {
+
+        return userRepository.search(userSearchCondition);
+    }
+
+    public Page<UserSearchResponseDto> searchPagedUsers(UserSearchCondition condition, Pageable pageable) {
+
+        return userRepository.searchPage(condition, pageable);
     }
 }
