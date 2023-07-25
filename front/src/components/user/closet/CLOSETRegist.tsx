ae859535421e2f87dadd943419bd0b4574bfa2f3
신산하 <tksgk2598@naver.com>
2023-07-25 10:45:13 +0900

    [feat]FE/closet/모달 컴포넌트 구현 완료

--- front/src/components/user/closet/CLOSETRegist.tsx
+++ front/src/components/user/closet/CLOSETRegist.tsx
@@ -2,18 +2,39 @@ import React, { useState, useRef, useCallback } from 'react';
 
 //css
 import closetRegistStyle from "../closet/CLOSETRegist.module.css";
+import { AnyAaaaRecord } from 'dns';
 
 const CLOSETRegist = () => {
 
 
     // 파일등록 관련 코드
     const inputRef = useRef<HTMLInputElement | null>(null);
+    const [imgSrc, setImgSrc] = useState<string>('');
 
-    const onUploadImage = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-        if (!e.target.files) {
+    const onUploadImage = useCallback((e: AnyAaaaRecord) => {
+        console.log(e);
+        if (!e) {
           return;
+        }else{ 
+            // file, Blob 객체를 핸들링하는데 사용
+            // File, Blob 객체를 사용해 특정 파일을 읽어들여 js에서 파일에 접근할 수 있게 도와줌
+            const reader = new FileReader();
+
+            // File 혹은 Blob 을 읽은 뒤 base64로 인코딩한 문자열을
+            //FileReader 인스턴스의 result라는 속성에 담아줌
+            reader.readAsDataURL(e);
+            console.log(reader);
+
+            return new Promise((resolve) => {
+                reader.onload = () => {       // FileReader가 성공적으로 파일을 읽어들였을 때 트리거 되는 이벤트 핸들러
+                                              // 이 내부에 우리가 원하는 로직을 넣어주면 됨
+                                              // 이번과 같은 경우는 setState로 img값 받기
+                    setImgSrc(reader.result);
+                    resolve();
+                };
+            });
+
         }
-        console.log(e.target.files[0].name);
     }, []);
 
     const onUploadImageButtonClick = useCallback(() => {
@@ -33,14 +54,31 @@ const CLOSETRegist = () => {
         { value: "bottom", name: "하의" },
         { value: "shoes", name: "신발" },
         { value: "accessory", name: "악세서리" },
-        { value: "total", name: "전체" },
     ];   
 
-    const [selected, setSelected] = useState<string>("상의");
+    const [selected, setSelected] = useState<string>("상의");//선택된 값을 저장
 
+    //select 값이 변하면 선택된 값을 변경
     const handleSelect = (e: any) => {
         setSelected(e.target.value);
     };
+
+    // 옷이름, 브랜드명, 특이사항 저장
+    const [clothesName, setClothesName] = useState<string>("");
+    const [clothesBrand, setClothesBrand] = useState<string>("");
+    const [specialContent, setSpecialContent] = useState<string>("");
+
+    const handleClothesName = (e:any) => {
+        setClothesName(e.target.value);
+    };
+
+    const handleClothesBrand = (e:any) => {
+        setClothesBrand(e.target.value);
+    };
+
+    const handleSpecialContent = (e:any) => {
+        setSpecialContent(e.target.value);
+    }
 
 
     return(
@@ -57,9 +95,9 @@ const CLOSETRegist = () => {
                 <div>
                     {/* 이미지 등록 */}
                     <div className={`${closetRegistStyle.imgRegist}`}>
-                        <input type="file" accept="image/*" ref={inputRef} onChange={onUploadImage} />
-                        {/* <button  onClick={onUploadImageButtonClick} >파일 업로드</button> */}
+                        {imgSrc?<img className={`${closetRegistStyle.img}`} src={imgSrc}/>:""}
                     </div>
+                    <input className={`${closetRegistStyle.selectFile}`} type="file" accept="image/*" ref={inputRef} onChange={(e)=>{onUploadImage(e.target.files[0])}} />
 
                     {/* input */}
                     <div>
@@ -79,27 +117,30 @@ const CLOSETRegist = () => {
                         </div>
 
                         {/* 옷이름 라인 */}
-                        <div className={closetRegistStyle.line} style={{marginTop:"-1%"}}>
+                        <div className={closetRegistStyle.line} onChange={handleClothesName} style={{marginTop:"-1%"}}>
                             <p>옷 이름 </p>
-                            <input style={{marginLeft:"2%"}} className={closetRegistStyle.select}></input>
+                            <input style={{marginLeft:"2%"}} className={closetRegistStyle.input}></input>
                         </div>
 
                         {/* 브랜드명 라인 */}
-                        <div className={closetRegistStyle.line} style={{marginTop:"-1%"}}>
+                        <div className={closetRegistStyle.line} onChange={handleClothesBrand} style={{marginTop:"-1%"}}>
                             <p>브랜드명 </p>
-                            <input className={closetRegistStyle.select}></input>
+                            <input className={closetRegistStyle.input}></input>
                         </div>
 
                         {/* 특이사항 라인 */}
-                        <div className={closetRegistStyle.line} style={{}}>
+                        <div className={closetRegistStyle.line} onChange={handleSpecialContent} style={{}}>
                             <p>특이사항</p>
-                            <input className={closetRegistStyle.select} style={{height:"100px"}}></input>
+                            <textarea className={closetRegistStyle.input} style={{height:"100px"}}/>
                         </div>
                     </div>
                 </div>
 
                 {/* 버튼 2개 */}
-                <div></div>
+                <div className={`${closetRegistStyle.buttons}`}>
+                    <button>취소</button>
+                    <button>업로드</button>
+                </div>
 
 
 
