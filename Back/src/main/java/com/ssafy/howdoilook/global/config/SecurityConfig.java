f1da5cbfcafa0d29e063d2ab978ced1d0f1f8000
ParkSeYun98 <yun12343@naver.com>
2023-07-28 12:12:22 +0900

    [fix] / back / securityconfig

--- Back/src/main/java/com/ssafy/howdoilook/global/config/SecurityConfig.java
+++ Back/src/main/java/com/ssafy/howdoilook/global/config/SecurityConfig.java
@@ -90,7 +90,7 @@ public class SecurityConfig {
                 // URL 별 권한 관리
                 .authorizeRequests()
 
-                .antMatchers("/", "/favicon.ico", "/api/user").permitAll()
+                .antMatchers("/", "/favicon.ico", "/api/swagger-ui/**").permitAll()
                 .antMatchers(PERMIT_URL_ARRAY).permitAll()
                 .antMatchers("/api/user/signup").permitAll() // 회원가입 접근 OK
                 .anyRequest().authenticated() // 그 외 경로는 모두 인증된 사용자만 접근 가능
