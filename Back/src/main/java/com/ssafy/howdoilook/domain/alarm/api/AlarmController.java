e6e0c33009721eedabaf230c943853e7e29e6ebd
Ray <tybam@naver.com>
2023-08-02 09:41:51 +0900

    [Update]BE/pageing/alarm pageing add

--- Back/src/main/java/com/ssafy/howdoilook/domain/alarm/api/AlarmController.java
+++ Back/src/main/java/com/ssafy/howdoilook/domain/alarm/api/AlarmController.java
@@ -4,6 +4,8 @@ import com.ssafy.howdoilook.domain.alarm.dto.request.AlarmSaveRequestDto;
 import com.ssafy.howdoilook.domain.alarm.dto.response.AlarmResponseDto;
 import com.ssafy.howdoilook.domain.alarm.service.AlarmService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,8 +18,8 @@ public class AlarmController {
     private final AlarmService alarmService;
 
     @GetMapping("/{userId}")
-    public ResponseEntity<?> selectAlarm(@PathVariable(name = "userId") Long userId){
-        List<AlarmResponseDto> alarmResponseDtoList = alarmService.selectAlarmByUserId(userId);
+    public ResponseEntity<?> selectAlarm(@PathVariable(name = "userId") Long userId, Pageable pageable){
+        Page<AlarmResponseDto> alarmResponseDtoList = alarmService.selectAlarmByUserId(userId, pageable);
         return ResponseEntity.ok().body(alarmResponseDtoList);
     }
     @PostMapping("/")
