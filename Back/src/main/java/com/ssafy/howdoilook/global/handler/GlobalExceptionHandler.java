d598ba0b6cb30512f7ecd14e29a6530dcc5436ec
Ray <tybam@naver.com>
2023-08-02 10:26:28 +0900

    [Update]BE/alarm/alarm controller return value,service exception update

--- Back/src/main/java/com/ssafy/howdoilook/global/handler/GlobalExceptionHandler.java
+++ Back/src/main/java/com/ssafy/howdoilook/global/handler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.ssafy.howdoilook.global.handler;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,6 +13,12 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleOtherException(Exception exception){
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(exception.getMessage());
+    }
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity<String> handleEmptyResultData(EmptyResultDataAccessException exception){
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
                 .body(exception.getMessage());
     }
 }
