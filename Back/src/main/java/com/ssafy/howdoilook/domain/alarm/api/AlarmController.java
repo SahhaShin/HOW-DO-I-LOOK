db31fed7e58b82ccd425cd09967300dc726a5d96
Ray <tybam@naver.com>
2023-08-03 16:43:27 +0900

    [Update]BE/Access/AlarmAccessUpdate

--- Back/src/main/java/com/ssafy/howdoilook/domain/alarm/api/AlarmController.java
+++ Back/src/main/java/com/ssafy/howdoilook/domain/alarm/api/AlarmController.java
@@ -32,13 +32,13 @@ public class AlarmController {
         return ResponseEntity.status(HttpStatus.CREATED).body(id);
     }
     @PutMapping("/{alarmId}")
-    public ResponseEntity<Long> readAlarm(@PathVariable(name = "alarmId") Long alarmId){
-        Long id = alarmService.readAlarm(alarmId);
+    public ResponseEntity<Long> readAlarm(@PathVariable(name = "alarmId") Long alarmId, @AuthenticationPrincipal UserDetails userDetails){
+        Long id = alarmService.readAlarm(alarmId,userDetails);
         return ResponseEntity.status(HttpStatus.CREATED).body(id);
     }
     @DeleteMapping("/{alarmId}")
-    public ResponseEntity<String> deleteAlarm(@PathVariable(name = "alarmId") Long alarmId) {
-        alarmService.deleteAlarm(alarmId);
+    public ResponseEntity<String> deleteAlarm(@PathVariable(name = "alarmId") Long alarmId, @AuthenticationPrincipal UserDetails userDetails) {
+        alarmService.deleteAlarm(alarmId,userDetails);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body("");
     }
 
