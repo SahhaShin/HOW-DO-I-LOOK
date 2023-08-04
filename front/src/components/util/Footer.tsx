7bf12fce43cd7711fc54d6ae67d4cbdadeb87d4f
신산하 <tksgk2598@naver.com>
2023-08-04 23:30:16 +0900

    [style]FE/feed/양 사이드 메뉴바 sticky

--- front/src/components/util/Footer.tsx
+++ front/src/components/util/Footer.tsx
@@ -5,9 +5,12 @@ import UtilStyle from "./Util.module.css";
 
 const Footer = () => {
   return (
-    <div>
-      <div><img src="#" alt="로고" /></div>
-      <div>How do I look</div>
+    <div className={`${UtilStyle.footer}`}>
+      <div className={`${UtilStyle.logos}`}>
+        <img className={`${UtilStyle.butterfly}`} src={process.env.PUBLIC_URL + `/img/logo2.png`} alt="HDIL" />
+        <img className={`${UtilStyle.logo}`} src={process.env.PUBLIC_URL + `/img/logo.png`} alt="HDIL" />
+      </div>
+      <div className={`${UtilStyle.name}`}>신산하 | 김은서 | 박세윤 | 손정민 | 정형준 | 유태영</div>
     </div>
   );
 };
