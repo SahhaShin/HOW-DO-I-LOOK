c35a0952b5693250625c6bb2534cc34f65ea3c2c
신산하 <tksgk2598@naver.com>
2023-08-06 05:27:41 +0900

    [feat]FE/feed/피드 등록할 때 이전, 다음 버튼 기능 가능, 마무리 버튼 클릭 시 전체 보기 가능

--- front/src/components/sns/feed/FeedCreate.tsx
+++ front/src/components/sns/feed/FeedCreate.tsx
@@ -12,6 +12,7 @@ import {action, changeFollow, changeCreateModalOpen} from "../../../store/FeedSl
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import { current } from '@reduxjs/toolkit';
 
 const FeedCreate = () => {
 
@@ -27,11 +28,24 @@ const FeedCreate = () => {
         slidesToScroll: 1,
     };
 
+    //유저정보
+    const userId = 1;
+
+    //사진 등록 시 피드 해시태그 관리
+    let [inputHash, setInputHash] = useState<string>("");//input에서 쓰고 있는 해시태그
+    let [hashtags, setHashtags] = useState<string[]>([]);
+    let [objectListHash, setObjectListHash] = useState([]);
+
     // 파일등록 관련 코드
     const inputRef = useRef<HTMLInputElement | null>(null);
-    const [imgSrc, setImgSrc] = useState<string>('');//미리보기 사진 url 저장용
-    const [imgSrcList, setImgSrcList] = useState<string[]>([]); //업로드하기로 결정한 이미지
-    const [imageFile, setImageFile] : any = useState();  //backend에 전달할 이미지 파일
+    
+    //현 이미지 src, file 상태 저장용
+    const [imgSrc, setImgSrc] = useState<string>('');//미리보기 사진 url 저장용, 업로드하기로 결정한 이미지
+    const [imgFile, setImaFile] = useState<any|null>(null);
+
+    //백엔드 전달용 리스트
+    const [imageSrcList, setImageSrcList] : string[] = useState([]);  //backend에 전달할 이미지 파일리스트
+    const [imageFileList, setImageFileList] : any[] = useState([]);  //backend에 전달할 이미지 파일리스트
 
     //사진추가 페이지 0부터 시작
     const [page, setPage] = useState<number>(0);
@@ -40,14 +54,86 @@ const FeedCreate = () => {
 
     // 이전 페이지로 이동
     function prevPage(){
-        setUploadEnd(false);
-        if(page>0) setPage(page-1);
+        if(page>0) {
+            setPage(page-1);
+            setUploadEnd(false);
+
+            //이전으로 이동한 페이지가 0보다 작지 않으면,
+            //이전에 등록했던 사진과 해시태그가 있다면 셋팅해준다.
+            if(page>=0){
+                setImgSrc(imageSrcList[page-1]);
+                setHashtags(objectListHash[page-1].hashtags); //null 문제
+            }
+        }
     }
 
     //다음 페이지로 이동
-    function nextPage(){
-        setUploadEnd(false);
-        if(page<5) setPage(page+1);
+    function nextPage(end:boolean){
+        // 페이지 이동 전에 사진이 등록되었는지 확인한다.
+        if(imgSrc===''){
+            alert('사진 등록은 필수입니다 :)');
+            return;
+        }
+
+        if(page<5) {
+            //다음 페이지가 이미 사진이 등록된 page라면 
+            //현재 페이지 정보를 저장하고, 다음 페이지 정보 가져오기
+            if(imageSrcList.length>page){
+
+                console.log(`여기 오케1 ${page}`);
+
+                // 사진 등록(백엔드에 보낼 사진리스트에)
+                const newImageSrcList = [...imageSrcList];
+                newImageSrcList.splice(page,1,imgSrc);
+                setImageSrcList(newImageSrcList);
+
+                const newImageFileList = [...imageFileList];
+                newImageFileList.splice(page,1,imgFile);
+                setImageFileList(newImageFileList);
+
+                // 해시태그 리스트에 등록 -> 리스트에 객체 형식으로 들어가야함
+                //해시태그 하나도 안달았을 때 빈리스트 들어가는지 확인해야함
+                const newObjectListHash = [...objectListHash];
+                newObjectListHash.splice(page,1,{hashtags});
+                setObjectListHash(newObjectListHash);
+
+
+                if(end) setUploadEnd(true);
+
+
+                setPage(page+1);
+
+                //다음페이지 정보 가져오기
+                console.log(`${imageSrcList.length} ${page+1}`);
+                if(imageSrcList.length>page+1){
+                    setUploadEnd(false);
+                    setImgSrc(imageSrcList[page+1]);
+                    setHashtags(objectListHash[page+1].hashtags); //null 문제 주의
+                }else{
+                    setImgSrc('');
+                    setImaFile(null);
+                    setHashtags([]);
+                }
+
+            }else{
+                //새로운 페이지라 사진을 등록해야함
+                setImageSrcList((imageSrcList)=>[...imageSrcList, imgSrc]);
+                setImageFileList((imageFileList)=>[...imageFileList, imgFile]);
+
+                //objectListHash = [{"여름", "더워"}]
+                const obj = {hashtags};//객체 안의 리스트로 만들기
+                setObjectListHash((objectListHash)=>[...objectListHash,obj]);
+
+                if(end) setUploadEnd(true);
+
+                setPage(page+1);
+                setUploadEnd(false);
+
+                setImgSrc('');
+                setImaFile(null);
+                setHashtags([]);
+            }
+        }
         else{
             alert('5개까지 업로드 가능합니다 :)');
             setUploadEnd(false);
@@ -55,10 +141,26 @@ const FeedCreate = () => {
         }
     }
 
+    //마지막 페이지로 이동
+    function endPage(){
+
+        if(page<5) {
+            nextPage(true);
+        }
+        //마지막 이전 페이지 내용까지 저장
+        
+
+        //전체 해시태그를 넣어줘야 한다.
+        for(let i=0;i<objectListHash.length;i++){
+            for(let j in objectListHash[i]){
+                setHashtags((hashtags)=>[...hashtags, objectListHash[i][j]]);
+            }
+        }
+        
+    }
 
     const onUploadImage = useCallback((file: any) => {
-       
-        setImageFile(file);
+        setImaFile(file);
         
         if (!file) {
           return;
@@ -70,7 +172,6 @@ const FeedCreate = () => {
             // File 혹은 Blob 을 읽은 뒤 base64로 인코딩한 문자열을
             //FileReader 인스턴스의 result라는 속성에 담아줌
             reader.readAsDataURL(file);
-            console.log(reader);
 
             return new Promise((resolve) => {
                 reader.onload = () => {       // FileReader가 성공적으로 파일을 읽어들였을 때 트리거 되는 이벤트 핸들러
@@ -91,16 +192,6 @@ const FeedCreate = () => {
         inputRef.current.click();
     }, []);
 
-    //사진 등록 관리
-    let [picture, setPicture] = useState<string[]|null>();
-
-    //유저정보
-    const userId = 1;
-
-    //사진 등록 시 피드 해시태그 관리
-    let [inputHash, setInputHash] = useState<string>("");
-    let [hashtags, setHashtags] = useState<string[]>([]);
-
 
     //피드 생성
     //해시태그 input 변화 감지 함수
@@ -108,11 +199,12 @@ const FeedCreate = () => {
         setInputHash(e.target.value);
     }
 
+    //추가버튼을 누르면 해시태그 리스트에 추가됨
     const addHashtag = (e) => {
         e.preventDefault();
         if(inputHash==="") return;
 
-        setHashtags((currentList)=>[inputHash, ...currentList]);
+        setHashtags((hashtags)=>[...hashtags, inputHash]);
 
         // dispatch(addHashtagList(inputHash));
         setInputHash("");
@@ -133,7 +225,12 @@ const FeedCreate = () => {
             s3upload:[]
         }
             dispatch(action.addFeed(newFeed));   
-        }
+    }
+
+
+    useEffect(()=>{
+        console.log("render");
+    },[page, uploadEnd, hashtags])
 
 
     
@@ -143,6 +240,17 @@ const FeedCreate = () => {
 
             {/* left */}
             {   uploadEnd?
+                <StyledSlider {...settings} className={`${feedCreateStyle.leftSlider}`}>
+                    {
+                        imageSrcList?.map((oneSrc,index)=>{
+                            return(
+                                <div className={`${feedCreateStyle.picture}`} key={index}>
+                                    <img className={`${feedCreateStyle.img}`} src={oneSrc}/>
+                                </div>
+                            );
+                        })
+                    }
+                </StyledSlider>:
                 <div className={`${feedCreateStyle.left}`}>
                     {/* 사진 등록 */}
                     <div className={`${feedCreateStyle.picture}`}>
@@ -153,19 +261,7 @@ const FeedCreate = () => {
                     </div>
                     {/* 파일선택 */}
                     <input className={`${feedCreateStyle.selectFile}`} type="file" accept="image/*" ref={inputRef} onChange={(e)=>{onUploadImage(e.target.files[0])}} />
-                </div>:
-
-                <StyledSlider {...settings} className={`${feedCreateStyle.left}`}>
-                    {
-                        imgSrcList?.map((oneSrc)=>{
-                            return(
-                                <div className={`${feedCreateStyle.picture}`}>
-                                    <img className={`${feedCreateStyle.img}`} src={oneSrc}/>
-                                </div>
-                            );
-                        })
-                    }
-                </StyledSlider>
+                </div>
 
 
             } 
@@ -181,6 +277,7 @@ const FeedCreate = () => {
                 {   uploadEnd?
                     <div className={`${feedCreateStyle.makeTag}`}>
                         <div>추가된 해시태그</div>
+                        <div className={`${feedCreateStyle.tags}`}>
                         {
                             hashtags?.map((one, idx)=>{
                                 return(
@@ -190,6 +287,7 @@ const FeedCreate = () => {
                                 );
                             })
                         }
+                        </div>
                     </div>:
                     <div className={`${feedCreateStyle.makeTag}`}>
                         <div>해시태그 생성</div>
@@ -212,7 +310,7 @@ const FeedCreate = () => {
                             {
                                 hashtags?.map((one, idx)=>{
                                     return(
-                                        <div className={`${feedCreateStyle.oneTag}`} key={idx} >
+                                        <div className={`${feedCreateStyle.oneTag2}`} key={idx} >
                                             #{one}
                                         </div>
                                     );
@@ -231,8 +329,8 @@ const FeedCreate = () => {
                     </div>:
                     <div className={`${feedCreateStyle.rightBtns}`}>
                         {page>0?<button onClick={()=>{prevPage()}}>이전</button>:null}
-                        <button onClick={()=>{nextPage()}}>사진추가</button>
-                        {page>=1?<button onClick={()=>{setUploadEnd(true)}}>마무리</button>:null}
+                        <button onClick={()=>{nextPage(false)}}>다음</button>
+                        <button onClick={()=>{endPage();}}>마무리</button>
                     </div>
                 }
             </div>
@@ -247,25 +345,26 @@ const StyledSlider = styled(Slider)`
   .slick-prev {
     z-index: 1;
     left: 30px;
-    top: 35%;
+    top: 50%;
   }
 
   .slick-next {
+    z-index: 1;
     right: 40px;
-    top: 35%;
+    top: 50%;
   }
 
   .slick-prev:before,
   .slick-next:before {
     font-size: 30px;
-    opacity: 0.5;
-    color: white;
+    opacity: 1;
+    color: black;
   }
 
   .slick-dots {
     display: flex;
     justify-content: center;
-    bottom: 200px;
+    bottom: 100px;
     color: black;
 
     li button:before {
