fdfae36465f12700ee7f121e56f21f6fcb88b4aa
eunseo <qnrqjrm03@naver.com>
2023-08-17 02:23:26 +0900

    [Update] FE/Intro/이것저것 수정

--- front/src/pages/user/login/Signin.tsx
+++ front/src/pages/user/login/Signin.tsx
@@ -4,10 +4,12 @@ import React, { useEffect, useState } from "react";
 import SingninStyle from "./Signin.module.css";
 
 //redux
-import { useSelector, useDispatch } from "react-redux"; 
+import { useSelector, useDispatch } from "react-redux";
 
 //컴포넌트
 import Signin from "../../../components/user/login/signin";
+
+import Header from "../../../components/util/Header";
 
 
 
@@ -15,18 +17,19 @@ import Signin from "../../../components/user/login/signin";
 const SigninPage = () => {
 
     //redux 관리
-    let state = useSelector((state:any)=>state.closet);
+    let state = useSelector((state: any) => state.closet);
     let dispatch = useDispatch();
 
     // 이미지 관리
 
 
-    return(
+    return (
         <div className={`${SingninStyle.wrapper}`}>
+            <Header></Header>
             <Signin></Signin>
 
         </div>
-        
+
     );
 }
 
