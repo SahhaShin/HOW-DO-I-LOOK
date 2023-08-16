62455650ebd643c83f05a3ec5141d347e906c11b
eunseo <qnrqjrm03@naver.com>
2023-08-17 01:01:38 +0900

    [Update] BE/Room/요구사항 수정

--- Back/src/main/java/com/ssafy/howdoilook/domain/roomUser/dto/response/RoomUserAddResponseDto.java
+++ Back/src/main/java/com/ssafy/howdoilook/domain/roomUser/dto/response/RoomUserAddResponseDto.java
@@ -9,11 +9,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class RoomUserAddResponseDto {
 
+    private String roomTitle;
     private String roomCode;
     private String chatCode;
 
     @Builder
-    public RoomUserAddResponseDto(String roomCode, String chatCode) {
+    public RoomUserAddResponseDto(String roomTitle, String roomCode, String chatCode) {
+        this.roomTitle = roomTitle;
         this.roomCode = roomCode;
         this.chatCode = chatCode;
     }
